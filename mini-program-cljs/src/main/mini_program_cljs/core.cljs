4b50d9dbfa39328e46c1f09f7eb3416d6f37b80f
chanshunli <chanshunli@gmail.com>
2020-02-29 19:35:48 +0800

    增加小程序的request方法

--- mini-program-cljs/src/main/mini_program_cljs/core.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/core.cljs
@@ -1,16 +1,16 @@
 (ns mini-program-cljs.core
   (:require
-   [mini-program-cljs.request :refer [request]]
+   [mini-program-cljs.request :refer [get post]]
    [mini-program-cljs.util :refer [alert]]
    [mini-program-cljs.login :refer [login get-user-info]]))
 
+(def ^:export Request #js {:get get :post post})
+
 (def ^:export
   MiniCljs (js-obj
-             "request"  request
              "alert" alert
              "login" login
-             "getUserInfo" get-user-info
-             "methodtest" #(+ % 100)))
+             "getUserInfo" get-user-info))
 
 (defn ^:export version [who]
   (str "0.1.4, " who "!"))
--- mini-program-cljs/src/main/mini_program_cljs/request.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/request.cljs
@@ -1,4 +1,31 @@
 (ns mini-program-cljs.request)
 
-(defn request [args]
-  (.request js/wx args))
+(defn promise
+  ([] js/Promise.)
+  ([callback] (js/Promise. callback)))
+
+(defn request
+  [{:keys [url method data token]}]
+  (promise
+    (fn [resolve reject]
+      (.request js/wx
+        #js {:url url
+             :method (name method)
+             :data (if (= method :GET)
+                     data
+                     (.stringify js/JSON data))
+             :header (merge {:Content-Type "application/json; charset=UTF-8"}
+                       (js->clj token))
+             :success (fn [^js request]
+                        (if (= 200 (.-code (.-data request)))
+                          (resolve (.-data request))
+                          (do
+                            (reject (.-data request)))))
+             :fail (fn [^js error]
+                     (reject (.-data error)))}))))
+
+(defn get [url params token]
+  (request {:url url :method :GET :data params :token token}))
+
+(defn post [url params token]
+  (request {:url url :method :POST :data params :token token}))
