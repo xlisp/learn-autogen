feb35c54f0caa66e81e5087912f44899e7165511
chanshunli <chanshunli@gmail.com>
2020-02-29 14:44:20 +0800

    需要用字符串才能引用到

--- mini-program-cljs/src/main/mini_program_cljs/core.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/core.cljs
@@ -2,8 +2,9 @@
   (:require [mini-program-cljs.request :refer [request]]))
 
 (def ^:export
-  Request (js-obj :request request
-            :methodtest #(+ %)))
+  MiniCljs (js-obj
+             "request"  request
+             "methodtest" #(+ %)))
 
 (defn ^:export version [who]
   (str "0.1.1, " who "!"))
