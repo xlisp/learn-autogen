650dd8dde762a1cbc2e3d84fbb86624bf52d9e18
chanshunli <chanshunli@gmail.com>
2020-02-29 14:35:56 +0800

    add js-obj export

--- mini-program-cljs/src/main/mini_program_cljs/core.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/core.cljs
@@ -1,5 +1,9 @@
 (ns mini-program-cljs.core
-  (:require [mini-program-cljs.request]))
+  (:require [mini-program-cljs.request :refer [request]]))
+
+(def ^:export
+  Request (js-obj :request request
+            :methodtest #(+ %)))
 
 (defn ^:export version [who]
   (str "0.1.1, " who "!"))
