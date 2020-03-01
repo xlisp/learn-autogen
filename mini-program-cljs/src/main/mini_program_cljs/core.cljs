1c1f6c2c0210730b96fceb214f96f736d2550b33
chanshunli <chanshunli@gmail.com>
2020-03-01 22:30:16 +0800

    对待你的库就像对待你的emacs配置一样, 写不出好的函数,哪怕改个自己能记住好点的名字alias都行

--- mini-program-cljs/src/main/mini_program_cljs/core.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/core.cljs
@@ -1,8 +1,10 @@
 (ns mini-program-cljs.core
   (:require
    [mini-program-cljs.request :refer [request]]
-   [mini-program-cljs.util :refer [alert switch-router]]
+   [mini-program-cljs.util :refer [alert switch-router set-storage-sync get-storage-sync]]
    [mini-program-cljs.login :refer [login get-user-info]]))
+
+(def ^:export Storage #js {:getSync get-storage-sync :setSync set-storage-sync})
 
 ;; (def ^:export Request #js {:get get :post post})
 
--- mini-program-cljs/src/main/mini_program_cljs/util.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/util.cljs
@@ -10,3 +10,12 @@
 (defn switch-router [url]
   (.navigateTo js/wx
     #js {:url url}))
+
+(defn set-storage-sync [key values]
+  (.setStorageSync js/wx
+    #js {:key key
+         :values (.stringify js/JSON values)}))
+
+(defn get-storage-sync [key]
+  (.parse js/JSON
+    (.getStorageSync js/wx key)))
