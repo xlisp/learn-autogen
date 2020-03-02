a272eb972c6c336ebf1f336f333376e5c2dcee09
chanshunli <chanshunli@gmail.com>
2020-03-02 11:20:35 +0800

    微信官方修改了的getUserInfo的方法,不能用了

--- mini-program-cljs/src/main/mini_program_cljs/login.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/login.cljs
@@ -1,23 +1,22 @@
 (ns mini-program-cljs.login
   (:require [mini-program-cljs.util :refer [alert]]))
 
+;; 微信官方接口的改变: 这个接口只能在开发环境,并且要点了用户授权button/getUserInfo之后才能用
 (defn get-user-info [success-fn fail-fn]
   (.getUserInfo js/wx
     #js {:success success-fn
          :fail-fn fail-fn}))
 
-(defn login [success-fn]
+(defn login [success-fn iv encrypted-data]
   (.login js/wx
     #js {:success
          (fn [^js r]
            (let [code (.-code r)]
              (if (empty? code)
                (alert (str "登录失败!" (.-errMsg r)))
-               (get-user-info
-                 (fn [^js res]
-                   (success-fn #js {:encryptedData (.-encryptedData res)
-                                    :iv (.-iv res)
-                                    :code code}))
-                 (fn [] (alert "获取用户信息失败!"))))
+               (success-fn #js {:encryptedData encrypted-data
+                                :iv iv
+                                :code code}))
              ))
-         :fail (fn [] false)}))
+         :fail (fn []
+                 (alert (str "登录失败!")))}))
