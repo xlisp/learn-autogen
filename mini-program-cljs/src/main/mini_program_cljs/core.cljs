027604cfac247b538e6b993a26fb411d912dd1e2
chanshunli <chanshunli@gmail.com>
2020-03-03 20:26:39 +0800

    增加https://github.com/roman01la/cljs-async-await的实现

--- mini-program-cljs/src/main/mini_program_cljs/core.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/core.cljs
@@ -1,8 +1,16 @@
 (ns mini-program-cljs.core
+  (:require-macros [mini-program-cljs.macro])
   (:require
    [mini-program-cljs.request :refer [request]]
    [mini-program-cljs.util :refer [alert switch-router set-storage-sync get-storage-sync]]
    [mini-program-cljs.login :refer [login get-user-info]]))
+
+(defn http-get-test [url]
+  (mini-program-cljs.macro/async
+    (let [response (mini-program-cljs.macro/await
+                     (js/setTimeout (fn [] (alert "定时结束")) 2000))
+          json (mini-program-cljs.macro/await #js {:data "定时器await"})]
+      (.log js/console json))))
 
 (def ^:export Storage #js {:getSync get-storage-sync :setSync set-storage-sync})
 
@@ -15,7 +23,8 @@
        :login login
        :switchRouter switch-router
        :getUserInfo get-user-info
-       :isEmpty empty?})
+       :isEmpty empty?
+       :awaitTest http-get-test})
 
 (defn ^:export version [who]
   (str "0.2.1, " who "!"))
