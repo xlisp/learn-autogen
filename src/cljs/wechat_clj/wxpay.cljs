65e0c48f978831651419b3e2d14ae4102b371880
chanshunli <chanshunli@gmail.com>
2020-03-09 12:47:49 +0800

    大坑: 后端的签名虽然正确,但是前端的timestamp的S是小写,还要加signType,签名才能正确

--- src/cljs/wechat_clj/wxpay.cljs
+++ src/cljs/wechat_clj/wxpay.cljs
@@ -1,16 +1,19 @@
 (ns wechat-clj.wxpay)
 
 (defn choose-wxpay
-  [{:keys [appid time-stamp nonce-str prepay-id
+  [{:keys [time-stamp nonce-str prepay-id
            sign-type pay-sign success-fn]
     :or {sign-type "MD5"
-         success-fn (fn [res] (js/alert (str "支付成功" res)))}}]
+         success-fn (fn [res] (js/alert (str "支付成功" res)))
+         cencel-fn (fn [res] (js/alert (str "支付取消" res)))
+         fail-fn (fn [res] (js/alert (str "支付失败" res)))}}]
   (.chooseWXPay
     js/wx
-    (clj->js {:appId appid
-              :timeStamp time-stamp
+    (clj->js {:timestamp time-stamp
               :nonceStr nonce-str
-              :package (str "prepay_id=" prepay-id)
               :signType sign-type
+              :package (str "prepay_id=" prepay-id)
               :paySign pay-sign
-              :success success-fn})))
+              :success success-fn
+              :cencel cencel-fn
+              :fail fail-fn})))
