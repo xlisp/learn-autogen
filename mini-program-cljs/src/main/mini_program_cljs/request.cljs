05c13be80445812b01e0d53052b61d840b972011
chanshunli <chanshunli@gmail.com>
2020-03-18 18:02:52 +0800

    defn-js是为request函数设计的 => 解决更友好的js api用户问题

--- mini-program-cljs/src/main/mini_program_cljs/request.cljs
+++ mini-program-cljs/src/main/mini_program_cljs/request.cljs
@@ -1,5 +1,5 @@
 (ns mini-program-cljs.request
-  (:require-macros [mini-program-cljs.macro :refer [defn-js]])
+  (:require-macros [mini-program-cljs.macro :refer [defn-js call-promise-1]])
   (:require [mini-program-cljs.util :refer [alert] :as u]
             [mini-program-cljs.js-wx :refer [js-wx]]))
 
@@ -8,19 +8,39 @@
     (merge {"Content-Type" "application/json; charset=UTF-8"}
       (js->clj headers))))
 
-(defn request [^js options]
-  (let [{:keys [url method data header]}
-        (u/jsx->clj options)]
-    (js/console.log url)
-    (js/Promise.
-      (fn [^js resolve ^js reject]
-        (js-wx "request"
-          #js {:url url
-               :method method
-               :data data
-               :header header
-               :success (fn [^js request]
-                          (js/console.log request)
-                          (resolve request))
-               :fail (fn [^js error]
-                       (reject error))})))))
+(comment
+  ;; 在控制台上面有显示出来: 参数和头部都是正确的, 但是没有alert出来结果
+  (js-wx "request"
+    #js {:url "https://www.test.com/testjson"
+         :method "GET"
+         :data #js {:aaa 111}
+         :header #js {"Content-Type" "application/json; charset=UTF-8"}
+         :success (fn [^js req]
+                    (js/console.log "request OK:" req)
+                    (alert (str req))
+                    )
+         :fail (fn [^js error]
+                 (js/console.log "request Error: " error))})
+  ;; 控制台有请求参数aaa但是没有头部信息aaaa
+  (call-promise-1
+    (fn [res]
+      (prn "请求返回:" res))
+    (request #js {:url "https://www.test.com/testjson"
+                  :method "GET"
+                  :data #js {:aaa 222}
+                  :headers (set-header {:token "aaaa"})})))
+(defn-js request
+  [:url :method :data :header]
+  (js/console.log url)
+  (js/Promise.
+    (fn [^js resolve ^js reject]
+      (js-wx "request"
+        #js {:url url
+             :method method
+             :data data
+             :header header
+             :success (fn [^js request]
+                        (js/console.log request)
+                        (resolve request))
+             :fail (fn [^js error]
+                     (reject error))}))))
